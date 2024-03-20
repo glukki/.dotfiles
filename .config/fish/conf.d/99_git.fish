b941a94f0416bd85dd28fae3cc9c3bb8b8aff74f
Vitalii (glukki) Meshchaninov <glukki.spb.ru@gmail.com>
2024-03-20 19:38:24 +0100

    Auto-squash (and fixup) only when rebasing on `master`

--- .config/fish/conf.d/99_git.fish
+++ .config/fish/conf.d/99_git.fish
@@ -14,8 +14,7 @@ abbr -a gpr         git pull-request
 abbr -a gprd        git pull-request -b dev
 abbr -a gprstage    git pull-request -b stage
 abbr -a grbi        git rebase --interactive --autostash
-abbr -a grbim       git rebase --interactive --autostash origin/main
-abbr -a grbis       git rebase --interactive --autostash origin/staging
+abbr -a grbim       git rebase --interactive --autostash --autosquash origin/main
 abbr -a gstau       git stash save --include-untracked
 abbr -a gcn!        git commit -v --no-edit --amend
 abbr -a gcf         git commit --fixup
