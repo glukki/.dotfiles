c96eee1729583a443403a99c6b9841bcfc8e0097
Vitalii (glukki) Meshchaninov <glukki.spb.ru@gmail.com>
2024-04-30 14:52:48 +0200

    Make radicle data backup prior to upgrade

--- bin/rad-upgrade.sh
+++ bin/rad-upgrade.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
+set -euo pipefail
+IFS=$'\n\t'
 
-echo Old version: $(rad --version)
-
+oldVersion=$(rad --version)
 needRestart=$(rad node status | grep "running")
 
 rad node stop
+tar czvf ~/radicle-backups_$(date +"%Y-%m-%d").tgz ~/.radicle
 curl -sSf https://radicle.xyz/install | sh
 
 echo
+echo Old version: $oldVersion
 echo New version: $(rad --version)
 
 if [[ -n "$needRestart" ]]; then
