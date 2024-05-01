a3447e431bf852d81e86d8a4946402c955e89757
Vitalii (glukki) Meshchaninov <glukki.spb.ru@gmail.com>
2024-05-01 19:09:17 +0200

    Fix `rad-upgrade` crash on node not running

--- bin/rad-upgrade.sh
+++ bin/rad-upgrade.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-needRestart=$(rad node status | grep "running")
+needRestart=$(rad node status | grep -c "running" || true)
 
 rad node stop
 tar czvf ~/radicle-backups_$(date +"%Y-%m-%d").tgz ~/.radicle
@@ -16,6 +16,6 @@ echo
 echo New version: $(rad --version)
 echo
 
-if [[ -n "$needRestart" ]]; then
+if (( $needRestart > 0 )); then
   rad node start
 fi
