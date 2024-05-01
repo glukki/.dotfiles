f878b53c80028deb6396457c6c86c96e06e9db2a
Vitalii (glukki) Meshchaninov <glukki.spb.ru@gmail.com>
2024-05-01 18:16:51 +0200

    Better versions output

--- bin/rad-upgrade.sh
+++ bin/rad-upgrade.sh
@@ -6,11 +6,15 @@ needRestart=$(rad node status | grep "running")
 
 rad node stop
 tar czvf ~/radicle-backups_$(date +"%Y-%m-%d").tgz ~/.radicle
+
+echo
 echo Old version: $(rad --version)
+
 curl -sSf https://radicle.xyz/install | sh
 
 echo
 echo New version: $(rad --version)
+echo
 
 if [[ -n "$needRestart" ]]; then
   rad node start
