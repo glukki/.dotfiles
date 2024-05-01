c6d19eca4b369d66959227cdddc2f49e1195af33
Vitalii (glukki) Meshchaninov <glukki.spb.ru@gmail.com>
2024-05-01 17:56:09 +0200

    Echo old radicle version before the upgrade

--- bin/rad-upgrade.sh
+++ bin/rad-upgrade.sh
@@ -2,15 +2,14 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-oldVersion=$(rad --version)
 needRestart=$(rad node status | grep "running")
 
 rad node stop
 tar czvf ~/radicle-backups_$(date +"%Y-%m-%d").tgz ~/.radicle
+echo Old version: $(rad --version)
 curl -sSf https://radicle.xyz/install | sh
 
 echo
-echo Old version: $oldVersion
 echo New version: $(rad --version)
 
 if [[ -n "$needRestart" ]]; then
