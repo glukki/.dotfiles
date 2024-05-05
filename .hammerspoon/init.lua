d49c147cec786c3ab1aa01184bfff37bcb271a9c
Vitalii (glukki) Meshchaninov <glukki.spb.ru@gmail.com>
2024-05-05 22:40:06 +0200

    Add Hammerspoon config

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,21 @@
+hs.loadSpoon("SpoonInstall")
+local SpoonInstall = spoon.SpoonInstall
+SpoonInstall:asyncUpdateAllRepos()
+
+SpoonInstall:andUse("ReloadConfiguration", { start = true })
+
+SpoonInstall:andUse("WindowGrid", {
+	config = {gridGeometries = {
+		{ "4x4" }
+	}},
+	hotkeys = { show_grid = { { "ctrl", "alt", "cmd" }, "g" } },
+	fn = function()
+		hs.grid.setMargins(hs.geometry.point(0, 0))
+	end,
+	start = true
+})
+
+-- open config file
+hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "c", function()
+	hs.execute("open ~/.hammerspoon/init.lua")
+end)
