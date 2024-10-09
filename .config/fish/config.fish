890b6af88c585d6315ccc55830baee8208973f8e
Vitalii (glukki) Meshchaninov <glukki.spb.ru@gmail.com>
2024-10-09 13:48:42 +0200

    Add `pnpm` dir to PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,9 @@
 ## dirs
 abbr -a cdp cd /Volumes/projects/
+
+# pnpm
+set -gx PNPM_HOME "/Users/glukki/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
