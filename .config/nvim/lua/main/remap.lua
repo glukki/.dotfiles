e9f1eaaf50206b7bab3777ab1dbe1d18a9f9911d
Vitalii (glukki) Meshchaninov <glukki.spb.ru@gmail.com>
2025-03-10 19:38:59 +0100

    Disable arows in normal mode in `nvim`

--- .config/nvim/lua/main/remap.lua
+++ .config/nvim/lua/main/remap.lua
@@ -12,3 +12,9 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
 vim.keymap.set("n", "Q", "<nop>")
+
+-- get better at moves
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
