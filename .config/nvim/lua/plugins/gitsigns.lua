89e2c78da88cefb90be59c1cd04051cfab5ff633
Vitalii (glukki) Meshchaninov <glukki.spb.ru@gmail.com>
2025-03-10 19:38:37 +0100

    Add `gitsigns` to `nvim`

---
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,12 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  opts = {
+    signs = {
+      add = { test = "+" },
+      change = { text = "~" },
+      delete = { text = "_" },
+      topdelete = { text = "‾" },
+      changedelete = { text = "~" },
+    },
+  },
+}
