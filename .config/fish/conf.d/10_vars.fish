1c7653b5e62f59b7906301088bc60ea3885d56bb
Vitalii (glukki) Meshchaninov <vital.mesh@gmail.com>
2025-10-02 16:39:23 +0200

    Fix fish init crash on missing secrets file

--- .config/fish/conf.d/10_vars.fish
+++ .config/fish/conf.d/10_vars.fish
@@ -1,12 +1,16 @@
 ## secret vars
-if not source ~/.secrets/secrets.fish
+
+set -l FILE ~/.secrets/secrets.fish
+
+if not test -e $FILE; or not source $FILE
   echo "Fish config is missing `secrets` file"
+  echo "touch $FILE"
 else
   set -l envVar
   for envVar in NPM_TOKEN HOMEBREW_GITHUB_API_TOKEN GEMFURY_TOKEN
     if not set -q $envVar
       echo Fish config is missing the "$envVar" variable. Set it with:
-      echo 'echo "set -gx' $envVar '\'value\'" >> ~/.secrets/secrets.fish'
+      echo 'echo "set -gx' $envVar '\'value\'" >> $FILE'
     end
   end
 end
